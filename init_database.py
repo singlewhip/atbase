4c17fc1bbe7b1b6de86fba709f0e7b096f5db9fb
singlewhip <51110971+singlewhip@users.noreply.github.com>
2020-06-29 22:31:50 +0800

    Update init_database.py

--- init_database.py
+++ init_database.py
@@ -3,7 +3,7 @@
 # @Author   : lc
 import MySQLdb
 
-conn = MySQLdb.Connect(host='47.103.53.14', port=int(3306), user='root', passwd='123456')
+conn = MySQLdb.Connect(host='127.0.0.1', port=int(3306), user='root', passwd='123456')
 db = 'atbase'
 cur = conn.cursor()
 if not cur.execute("SELECT * FROM information_schema.SCHEMATA where SCHEMA_NAME='" + db + "'"):
